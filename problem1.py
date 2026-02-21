ca071ca62b1428d6557298b30e097d8fc2c5cb82
sjulian4 <sarah.julian@stonybrook.edu>
2026-02-21 14:04:39 -0500

    setting up files

---
+++ problem1.py
@@ -0,0 +1,3 @@
+# (1) (20 pts) Rectangle method (left, right, or midpoint; specify which).
+# (2) (20 pts) Trapezoidal method.
+# (3) (10 pts) Monte Carlo method (note: this may run significantly slower than the first two).
---
+++ problem2.py
@@ -0,0 +1,2 @@
+# (1) (30 pts) Solving the Linear System: Write a computer program to solve the linear system Ax = b, where A is the given coefficient matrix and b is the given right-hand side vector with N = 126.
+# (2) (20 pts) Verifying the Solution: Write a computer program (or extend the program from (1)) to verify the accuracy of your solution x by computing the L1 residual norm |AX − b|_1
