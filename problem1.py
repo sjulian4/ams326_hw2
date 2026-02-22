c7b1f7aab102062947a28f778ea2abc64fe75578
sjulian4 <sarah.julian@stonybrook.edu>
2026-02-22 15:10:16 -0500

    setup

--- problem1.py
+++ problem1.py
@@ -1,3 +1,23 @@
+import numpy as np
+
+# Kidney equation: (x^2 + y^2)^2 = x^3 + y^3
+# Removed disc: (x - 0.25)^2 + (y - 0.25)^2 = 0.125
+
+
 # (1) (20 pts) Rectangle method (left, right, or midpoint; specify which).
+print("Rectangle Method. \nUsing the midpoint method.")
+
+# delta x * [f(x_1^*) + ... + f(x_n^*)]
+
+
+
 # (2) (20 pts) Trapezoidal method.
+print("Trapezoioal Method")
+
+
+
+
 # (3) (10 pts) Monte Carlo method (note: this may run significantly slower than the first two).
+print("Monte Carlo Method")
+
+
