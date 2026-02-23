87d8ebb8f4ce9b0c23de6da579b83b980a5eb61d
sjulian4 <sarah.julian@stonybrook.edu>
2026-02-23 14:22:40 -0500

    rectangle method

--- problem1.py
+++ problem1.py
@@ -3,12 +3,46 @@ import numpy as np
 # Kidney equation: (x^2 + y^2)^2 = x^3 + y^3
 # Removed disc: (x - 0.25)^2 + (y - 0.25)^2 = 0.125
 
+# these functions are helpers with determining the regions
+def inside_kidney(x, y):
+    return (x**2 + y**2)**2 <= x**3 + y**3
+
+def inside_disc(x, y):
+    return (x - 0.25)**2 + (y - 0.25)**2 <= 0.125
+
+def integrand(x, y):
+    # 1 if inside kidney AND not inside disc
+    return inside_kidney(x, y) and not inside_disc(x, y)
+
+
+
+
+# bounding box (covers entire kidney)
+xmin, xmax = -0.5, 1.5
+ymin, ymax = -0.5, 1.5
 
 # (1) (20 pts) Rectangle method (left, right, or midpoint; specify which).
 print("Rectangle Method. \nUsing the midpoint method.")
 
 # delta x * [f(x_1^*) + ... + f(x_n^*)]
+# need to go above and below the x axis
+def rectangle_method(n):
+    dx = (xmax - xmin) / n
+    dy = (ymax - ymin) / n
+    area = 0.0
+    
+    for i in range(n):
+        for j in range(n):
+            x = xmin + (i + 0.5) * dx
+            y = ymin + (j + 0.5) * dy
+            if integrand(x, y):
+                area += dx * dy
+                
+    return area
 
+n_rect = 1000 # 1000 subintervals
+area_rect = rectangle_method(n_rect)
+print("Rectangle (midpoint) area:", area_rect)
 
 
 # (2) (20 pts) Trapezoidal method.
